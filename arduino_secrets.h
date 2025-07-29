8ca187fe16b99542c656ea1ca23f480b686c5177
Sydney <sydneyjohns@vt.edu>
2025-07-29 11:14:06 -0400

    Create arduino_secrets.h
    add secret header

---
+++ arduino_secrets.h
@@ -0,0 +1,10 @@
+// arduino_secrets.h
+#ifndef ARDUINO_SECRETS_H
+#define ARDUINO_SECRETS_H
+
+const char* username = "alice";
+const char* password = "P@ssw0rd123";
+
+String credentials = String("Username: ") + username + ", Password: " + password;
+
+#endif
